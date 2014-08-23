77e1c12f947f815a51aa083d3949f9468221af20
Tom VanAntwerp <vanantwerp@gmail.com>
2014-08-23 14:37:34 -0400

    Beginning of space scene
    Added basic spaceship sprite and animation, some player control,
    background and broken scrolling

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipController : MonoBehaviour {
+
+	public float maxShipSpeed = 20f;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		float sideMotion = Input.GetAxis ("Horizontal");
+		float vertMotion = Input.GetAxis ("Vertical");
+
+		pos.y += vertMotion * Time.deltaTime * maxShipSpeed;
+		pos.x += sideMotion * Time.deltaTime * maxShipSpeed;
+
+		transform.position = pos;
+	}
+}
