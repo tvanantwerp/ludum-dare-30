fe522276de65d21714d1c6e1040049afcf631e44
Tom VanAntwerp <vanantwerp@gmail.com>
2014-08-24 16:09:50 -0400

    Add parallaxing, cloud movement, more level

---
+++ Assets/Scripts/SkyRearTiling.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyRearTiling : MonoBehaviour {
+	public float defaultWidth = 81.92f;
+	public float defaultTiles = 4f;
+	
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.name == "sky") {
+			Vector2 pos = collider.transform.position;
+			pos.x += defaultWidth * defaultTiles;
+			collider.transform.position = pos;
+		} 
+	}
+}
